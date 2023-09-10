95ac92ece7a760dd5c6b61f05d08e71ad63b2406
Rinat Zaripov <z_renat@inbox.ru>
2023-09-10 21:20:41 +0600

    Initial Commit

---
+++ swiftui project/swiftui_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_projectApp.swift
+//  swiftui project
+//
+//  Created by Rinat Zaripov on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
